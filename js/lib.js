2eeb34a460a8d871d7aaa48a1438684bbfdeebdb
BiAniTy <drayttc@gmail.com>
2022-04-05 21:27:27 -0300

    Add comments

--- js/lib.js
+++ js/lib.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => main());
 
+// Book constructor
 function Book(title, author, pages, have_read) {
     this.title = title;
     this.author = author;
@@ -7,21 +8,30 @@ function Book(title, author, pages, have_read) {
     this.have_read = have_read;
 }
 
+// Adds a book to the display after filling the form
 function add_book_to_library() {
+    // The library container
     let lib = document.querySelector(".library");
     let book_card;
 
+    // When submitting the form
     let submit_book = document.querySelector("#submit_book");
     submit_book.addEventListener("click", (e) => {
+        // Removes all book inside the library
         purge_all_children(lib);
 
+        // Iterates over each book inside the library array
         library.forEach((book, index) => {
+            // Creates the book element and sets its class and attribute
             book_card = document.createElement("div");
             book_card.classList.add("book");
             book_card.setAttribute("data_id", `${index}`);
 
+            // For convenience
             let sections = [];
 
+            // Creates each section inside the book element
+            // and set its value according to the books inside the array
             let title_section = document.createElement("div");
             title_section.setAttribute("data-title", "");
             title_section.textContent = book.title;
@@ -46,15 +56,18 @@ function add_book_to_library() {
             delete_book_btn.textContent = "Delete book";
             sections.push(delete_book_btn);
 
+            // Attaches all sections to the book element
             sections.forEach((section) => {
                 book_card.appendChild(section);
             });
 
+            // Finally, adds the book to the library
             lib.appendChild(book_card);
         });
     });
 }
 
+// The library array, containing all Book objects
 let library = [];
 function main() {
     manage_add_book_panel();
@@ -62,6 +75,7 @@ function main() {
     add_book_to_library();
 }
 
+// Controls the functionality of the add_book button and form
 function manage_add_book_panel() {
     let add_book_btn = document.querySelectorAll(".add_book_btn");
 
@@ -69,8 +83,10 @@ function manage_add_book_panel() {
         "[data_close_button]"
     );
 
+    // Overlay to cover all the screen while the form is up
     let overlay = document.querySelector("#overlay");
 
+    // Opens the form panel when clicking the add_book button
     add_book_btn.forEach((button) =>
         button.addEventListener("click", () => {
             const panel = document.querySelector("#add_book_panel");
@@ -78,6 +94,7 @@ function manage_add_book_panel() {
         })
     );
 
+    // Closes the form panel when clicking the closing button
     close_add_book_panel_btn.forEach((button) =>
         button.addEventListener("click", () => {
             const panel = button.closest(".add_book_panel");
@@ -85,6 +102,7 @@ function manage_add_book_panel() {
         })
     );
 
+    // Manages the changes related to the overlay layer
     overlay.addEventListener("click", () => {
         const panels = document.querySelectorAll(".add_book_panel.active");
 
@@ -100,6 +118,8 @@ function manage_add_book_panel() {
 // TODO: Add form validation
 function get_panel_input() {
     let form = document.querySelector("#form");
+
+    // Input required for the new book
     let title;
     let author;
     let pages;
@@ -108,8 +128,10 @@ function get_panel_input() {
     let submit_book = document.querySelector("#submit_book");
 
     submit_book.addEventListener("click", (e) => {
+        // Stop the buttom from actually submitting and reloading the page
         e.preventDefault();
 
+        // Collects the inputs
         let book_data = new FormData(form);
         title = book_data.get("title");
         author = book_data.get("author");
@@ -119,14 +141,19 @@ function get_panel_input() {
 
         checkbox.checked ? (have_read = true) : (have_read = false);
 
+        // Creates a new Book object based on the user inputs
         let book = new Book(title, author, pages, have_read);
+
+        // Adds the book to the library array
         library.push(book);
 
+        // Closes the panel afterwards
         const panel = document.querySelector(".add_book_panel");
         close_panel(panel);
     });
 }
 
+// Opens the add_book form panel
 function open_panel(panel) {
     if (panel == null) return;
 
@@ -134,6 +161,7 @@ function open_panel(panel) {
     overlay.classList.add("active");
 }
 
+// Closes the add_book form panel
 function close_panel(panel) {
     if (panel == null) return;
 
@@ -141,6 +169,7 @@ function close_panel(panel) {
     overlay.classList.remove("active");
 }
 
+// Removes all children from a parent element
 function purge_all_children(parent) {
     while (parent.hasChildNodes()) {
         parent.removeChild(parent.lastChild);
